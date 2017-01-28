d5d1bc43d3166aaabe3058c9aa1ef86571ecc08e
Ruffeng <ruben.suet@gmail.com>
2017-01-28 18:47:56 +0100

    Background scrolling. Time to figure out how to make it pretty

--- wagon.rb
+++ wagon.rb
@@ -9,12 +9,22 @@ class Wagon < Gosu::Window
     @background = background
     @player = player
     @player.warp(170,352)
+    #Scrolling effect
+    @x_back = @y_back = 0
   end
   def update
+    @x_back -= 10
+    @coordinates = Gosu::Image.from_text(
+      self, "x: #{@x_back} - loc: #{@local_x} bgwidth : #{@background.width}",Gosu.default_font_name, 30)
+
   end
 
   def draw
+    @local_x = @x_back % -640
     @player.draw
-    @background.draw(0,0,0)
+    if @local_x < 0
+      @background.draw(@local_x + 640,0,0)
+    end
+    @coordinates.draw(0,0,1)
   end
 end
